65a9e3d7fd7ead7ca4b85fd39e2b48129dfedcfa
ArtemBorodinEvgenyevich <artem@e-boroda.org>
2020-06-04 22:30:01 +0500

    Test fix

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,6 +1,7 @@
+import os
 import sys
 import unittest
-from disk_usage import main
+from disk_usage import main, write_to_file
 
 
 class TestMain(unittest.TestCase):
@@ -8,6 +9,19 @@ class TestMain(unittest.TestCase):
     def test_main(self):
         sys.argv = ['-e', '.p']
         self.assertRaises(FileNotFoundError, main)
+
+    def test_write_to_file(self):
+        raised = False
+        path = os.path.dirname(__file__)
+
+        try:
+            write_to_file(path=path, tablefmt="simple",
+                          tables=[], headers=[], colalign=[])
+        except:
+            raised = True
+
+        self.assertFalse(raised, "Exception raised")
+
 
 
 if __name__ == '__main__':
